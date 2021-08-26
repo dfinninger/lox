2c36491b093fa00b611dcf9f6eab6db5d6483752
Devon Finninger <devonfinninger@gmail.com>
2021-08-25 20:43:48 -0700

    add statements to the interpreter

--- jlox/src/main/java/com/dfinninger/lox/Parser.java
+++ jlox/src/main/java/com/dfinninger/lox/Parser.java
@@ -1,11 +1,13 @@
 package com.dfinninger.lox;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static com.dfinninger.lox.TokenType.*;
 
 public class Parser {
-    private static class ParseError extends RuntimeException {}
+    private static class ParseError extends RuntimeException {
+    }
 
     private final List<Token> tokens;
     private int current = 0;
@@ -14,16 +16,35 @@ public class Parser {
         this.tokens = tokens;
     }
 
-    Expr parse() {
-        try {
-            return expression();
-        } catch (ParseError error) {
-            return null;
+    List<Stmt> parse() {
+        List<Stmt> statements = new ArrayList<>();
+        while (!isAtEnd()) {
+            statements.add(statement());
         }
+
+        return statements;
     }
 
     private Expr expression() {
         return equality();
+    }
+
+    private Stmt statement() {
+        if (match(PRINT)) return printStatement();
+
+        return expressionStatement();
+    }
+
+    private Stmt printStatement() {
+        Expr value = expression();
+        consume(SEMICOLON, "Expect ';' after value.");
+        return new Stmt.Print(value);
+    }
+
+    private Stmt expressionStatement() {
+        Expr expr = expression();
+        consume(SEMICOLON, "Expect ';' after value.");
+        return new Stmt.Expression(expr);
     }
 
     private Expr equality() {
