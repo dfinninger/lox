17b3424219dbacd22eb2395dba01d1e0bfab8dff
Devon Finninger <devonfinninger@gmail.com>
2021-08-29 16:26:39 -0700

    fix REPL linebreak

--- jlox/src/main/java/com/dfinninger/lox/Lox.java
+++ jlox/src/main/java/com/dfinninger/lox/Lox.java
@@ -42,7 +42,7 @@ public class Lox {
         BufferedReader reader = new BufferedReader(input);
 
         for (; ; ) {
-            System.out.println("> ");
+            System.out.print("> ");
             String line = reader.readLine();
             if (line == null) break;
             run(line);
